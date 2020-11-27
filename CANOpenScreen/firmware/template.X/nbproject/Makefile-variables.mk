b507858d04ec8aeda7322b08517ac3d1ecd4bb10
Elco Cuijpers <elcocuijpers12@msn.com>
2020-11-27 13:59:48 +0100

    Empty project created

---
+++ CANOpenScreen/firmware/template.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# CANOpenScreenConfig configuration
+CND_ARTIFACT_DIR_CANOpenScreenConfig=dist/CANOpenScreenConfig/production
+CND_ARTIFACT_NAME_CANOpenScreenConfig=template.X.production.hex
+CND_ARTIFACT_PATH_CANOpenScreenConfig=dist/CANOpenScreenConfig/production/template.X.production.hex
+CND_PACKAGE_DIR_CANOpenScreenConfig=${CND_DISTDIR}/CANOpenScreenConfig/package
+CND_PACKAGE_NAME_CANOpenScreenConfig=template.x.tar
+CND_PACKAGE_PATH_CANOpenScreenConfig=${CND_DISTDIR}/CANOpenScreenConfig/package/template.x.tar
